7bb18f4eecfa1ad37e1730d89f14f4d570a03418
emranaloul <emranaloul1992@hotmail.com>
2025-03-09 22:38:32 +0300

    Fix import paths in main.tsx and update Vite configuration

--- src/main.tsx
+++ src/main.tsx
@@ -21,9 +21,9 @@ import './index.css';
 // Material Dashboard 2 React Context Provider
 import { MaterialUIControllerProvider } from './context';
 import { Provider } from 'react-redux';
-import { store } from './store';
-import App from './App';
-import Loader from './components/Loader';
+import { store } from 'store';
+import App from 'App';
+import Loader from 'components/Loader';
 
 const container = document.getElementById('app');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
